35061bc6218a165373e96972b1a72e8db522c859
samaneh <samanehcosmonaut@gmail.com>
2022-08-22 08:24:39 +0430

    Included view.py and urls.py from Zarinpal sample code into the Open edX's ecommmerce

--- ecommerce/extensions/payment/urls.py
+++ ecommerce/extensions/payment/urls.py
@@ -1,40 +1,69 @@
-
-
 from django.conf import settings
 from django.conf.urls import include, url
 
-from ecommerce.extensions.payment.views import PaymentFailedView, SDNFailure, cybersource, paypal, stripe
+from ecommerce.extensions.payment.views import (
+    PaymentFailedView,
+    SDNFailure,
+    cybersource,
+    paypal,
+    stripe,
+    zarinpal,
+)
 
 CYBERSOURCE_APPLE_PAY_URLS = [
-    url(r'^authorize/$', cybersource.CybersourceApplePayAuthorizationView.as_view(), name='authorize'),
-    url(r'^start-session/$', cybersource.ApplePayStartSessionView.as_view(), name='start_session'),
+    url(
+        r"^authorize/$",
+        cybersource.CybersourceApplePayAuthorizationView.as_view(),
+        name="authorize",
+    ),
+    url(
+        r"^start-session/$",
+        cybersource.ApplePayStartSessionView.as_view(),
+        name="start_session",
+    ),
 ]
 CYBERSOURCE_URLS = [
-    url(r'^apple-pay/', include((CYBERSOURCE_APPLE_PAY_URLS, 'apple_pay'))),
-    url(r'^authorize/$', cybersource.CybersourceAuthorizeAPIView.as_view(), name='authorize'),
+    url(r"^apple-pay/", include((CYBERSOURCE_APPLE_PAY_URLS, "apple_pay"))),
+    url(
+        r"^authorize/$",
+        cybersource.CybersourceAuthorizeAPIView.as_view(),
+        name="authorize",
+    ),
 ]
 
 PAYPAL_URLS = [
-    url(r'^execute/$', paypal.PaypalPaymentExecutionView.as_view(), name='execute'),
-    url(r'^profiles/$', paypal.PaypalProfileAdminView.as_view(), name='profiles'),
+    url(r"^execute/$", paypal.PaypalPaymentExecutionView.as_view(), name="execute"),
+    url(r"^profiles/$", paypal.PaypalProfileAdminView.as_view(), name="profiles"),
 ]
 
 SDN_URLS = [
-    url(r'^failure/$', SDNFailure.as_view(), name='failure'),
+    url(r"^failure/$", SDNFailure.as_view(), name="failure"),
 ]
 
 STRIPE_URLS = [
-    url(r'^submit/$', stripe.StripeSubmitView.as_view(), name='submit'),
+    url(r"^submit/$", stripe.StripeSubmitView.as_view(), name="submit"),
+]
+
+ZARINPAL_URLS = [
+    url(r"^request/$", zarinpal.send_request, name="request"),
+    url(r"^verify/$", zarinpal.verify, name="verify"),
 ]
 
 urlpatterns = [
-    url(r'^cybersource/', include((CYBERSOURCE_URLS, 'cybersource'))),
-    url(r'^error/$', PaymentFailedView.as_view(), name='payment_error'),
-    url(r'^paypal/', include((PAYPAL_URLS, 'paypal'))),
-    url(r'^sdn/', include((SDN_URLS, 'sdn'))),
-    url(r'^stripe/', include((STRIPE_URLS, 'stripe'))),
+    url(r"^cybersource/", include((CYBERSOURCE_URLS, "cybersource"))),
+    url(r"^error/$", PaymentFailedView.as_view(), name="payment_error"),
+    url(r"^paypal/", include((PAYPAL_URLS, "paypal"))),
+    url(r"^sdn/", include((SDN_URLS, "sdn"))),
+    url(r"^stripe/", include((STRIPE_URLS, "stripe"))),
+    url(r"^zarinpal/", include((ZARINPAL_URLS, "zarinpal"))),
 ]
 
-for payment_processor_name, urls_module in settings.EXTRA_PAYMENT_PROCESSOR_URLS.items():
-    processor_url = url(r'^{}/'.format(payment_processor_name), include((urls_module, payment_processor_name)))
+for (
+    payment_processor_name,
+    urls_module,
+) in settings.EXTRA_PAYMENT_PROCESSOR_URLS.items():
+    processor_url = url(
+        r"^{}/".format(payment_processor_name),
+        include((urls_module, payment_processor_name)),
+    )
     urlpatterns.append(processor_url)
---
+++ ecommerce/extensions/payment/views/zarinpal.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# Github.com/Rasooll
+from django.http import HttpResponse
+from django.shortcuts import redirect
+import requests
+import json
+
+MERCHANT = "18722589-4522-1785-2795-757521498875"
+ZP_API_REQUEST = "https://sandbox.zarinpal.com/pg/v4/payment/request.json"
+ZP_API_VERIFY = "https://sandbox.zarinpal.com/pg/v4/payment/verify.json"
+ZP_API_STARTPAY = "https://sandbox.zarinpal.com/pg/StartPay/{authority}"
+amount = 11000  # Rial / Required
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+email = "email@example.com"  # Optional
+mobile = "09123456789"  # Optional
+# Important: need to edit for realy server.
+CallbackURL = "http://edx.scischool.ir/verify/"
+
+
+def send_request(request):
+    req_data = {
+        "merchant_id": MERCHANT,
+        "amount": amount,
+        "callback_url": CallbackURL,
+        "description": description,
+        "metadata": {"mobile": mobile, "email": email},
+    }
+    req_header = {"accept": "application/json", "content-type": "application/json'"}
+    req = requests.post(
+        url=ZP_API_REQUEST, data=json.dumps(req_data), headers=req_header
+    )
+    authority = req.json()["data"]["authority"]
+    if len(req.json()["errors"]) == 0:
+        return redirect(ZP_API_STARTPAY.format(authority=authority))
+    else:
+        e_code = req.json()["errors"]["code"]
+        e_message = req.json()["errors"]["message"]
+        return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+
+
+def verify(request):
+    t_status = request.GET.get("Status")
+    t_authority = request.GET["Authority"]
+    if request.GET.get("Status") == "OK":
+        req_header = {"accept": "application/json", "content-type": "application/json'"}
+        req_data = {"merchant_id": MERCHANT, "amount": amount, "authority": t_authority}
+        req = requests.post(
+            url=ZP_API_VERIFY, data=json.dumps(req_data), headers=req_header
+        )
+        if len(req.json()["errors"]) == 0:
+            t_status = req.json()["data"]["code"]
+            if t_status == 100:
+                return HttpResponse(
+                    "Transaction success.\nRefID: " + str(req.json()["data"]["ref_id"])
+                )
+            elif t_status == 101:
+                return HttpResponse(
+                    "Transaction submitted : " + str(req.json()["data"]["message"])
+                )
+            else:
+                return HttpResponse(
+                    "Transaction failed.\nStatus: " + str(req.json()["data"]["message"])
+                )
+        else:
+            e_code = req.json()["errors"]["code"]
+            e_message = req.json()["errors"]["message"]
+            return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+    else:
+        return HttpResponse("Transaction failed or canceled by user")
